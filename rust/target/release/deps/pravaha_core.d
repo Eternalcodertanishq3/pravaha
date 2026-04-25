737bb2af9b4c3dd1b9d60b6c532b82878d7fdbb3
Your Name <eternitydark84@gmail.com>
2026-04-25 23:48:43 +0530

    chore: remove accidentally committed Rust build artifacts

--- rust/target/release/deps/pravaha_core.d
+++
@@ -1,6 +0,0 @@
-C:\Personal Projects\Pravāha\rust\target\release\deps\pravaha_core.d: src\lib.rs src\allocator.rs
-
-C:\Personal Projects\Pravāha\rust\target\release\deps\pravaha_core.dll: src\lib.rs src\allocator.rs
-
-src\lib.rs:
-src\allocator.rs:
--- rust/target/release/pravaha_core.d
+++
@@ -1,1 +0,0 @@
-C:\Personal\ Projects\Pravāha\rust\target\release\pravaha_core.dll: C:\Personal\ Projects\Pravāha\rust\src\allocator.rs C:\Personal\ Projects\Pravāha\rust\src\lib.rs
